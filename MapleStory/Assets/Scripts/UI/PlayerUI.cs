c29d2c683645a8370704ab1e499a69d74ff69b73
Bae-Seung-Hwan <59654788+Bae-Seung-Hwan@users.noreply.github.com>
2025-07-06 04:53:57 +0900

    체력포션과 마나포션 사용시 회복

--- MapleStory/Assets/Scripts/UI/PlayerUI.cs
+++ MapleStory/Assets/Scripts/UI/PlayerUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class PlayerUI : MonoBehaviour
 {
@@ -13,6 +14,52 @@ public class PlayerUI : MonoBehaviour
     public TextMeshProUGUI hpText;
     public TextMeshProUGUI mpText;
     public TextMeshProUGUI expText;
+
+    float currentHP;
+    float currentMP;
+    float currentEXP;
+    float maxHP = 100f;
+    float maxMP = 100f;
+    float maxEXP = 100f;
+
+    void Start()
+    {
+        currentHP = 70f;
+        currentMP = 60f;
+        currentEXP = 100f;
+
+        UpdateHP(currentHP, maxHP);
+        UpdateMP(currentMP, maxMP);
+        UpdateEXP(currentEXP, maxEXP);
+    }
+
+    void Update()
+    {
+
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            UseHealthPotion();
+        }
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            UseManaPotion();
+        }
+    }
+
+    public void UseHealthPotion()
+    {
+        float healAmount = 30f;
+        currentHP = Mathf.Min(currentHP + healAmount, maxHP);
+        UpdateHP(currentHP, maxHP);
+        Debug.Log("체력 포션 사용됨!");
+    }
+    public void UseManaPotion()
+    {
+        float recoverAmount = 20f;
+        currentMP = Mathf.Min(currentMP + recoverAmount, maxMP);
+        UpdateMP(currentMP, maxMP);
+        Debug.Log("마나 포션 사용됨!");
+    }
 
     public void UpdateHP(float current, float max)
     {
@@ -30,12 +77,5 @@ public class PlayerUI : MonoBehaviour
     {
         expSlider.value = current / max;
         expText.text = $"{(int)current} / {(int)max}";
-    }
-
-    void Start()
-    {
-        UpdateHP(70, 100);
-        UpdateMP(60, 60);
-        UpdateEXP(150, 150);
     }
 }
