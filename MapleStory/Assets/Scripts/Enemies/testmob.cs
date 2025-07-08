a9ba4e92264c33bfbf86984217f449ccaba4af92
dongheon3 <kdh0304h@gmail.com>
2025-07-08 17:47:21 +0900

    데미지 폰트 출력
    - 데미지 입을 시 데미지 폰트 출력

--- MapleStory/Assets/Scripts/Enemies/testmob.cs
+++ MapleStory/Assets/Scripts/Enemies/testmob.cs
@@ -4,6 +4,8 @@ public class testmob : MonoBehaviour
 {
     public MonsterData data;
     public string monsterName;
+    public GameObject DamageTextPrefab;
+
     private float monsterLevel;
     private float currentHealth;
     private bool isdead = false;
@@ -11,24 +13,26 @@ public class testmob : MonoBehaviour
     public MonsterSpawner spawner;
     private Animator animator;
 
+
     void Start()
     {
         animator = GetComponent<Animator>();
         currentHealth = data.maxHealth;
         monsterName = data.monsterName;
         monsterLevel = data.monsterLevel;
-        Die();
+
+        StartCoroutine(DamageOverTime());
+
     }
 
     void Die()
     {
-        
-        StartCoroutine(MobDieAimationDelay(4f));
+        StartCoroutine(MobDieAimationDelay(0f));
 
 
         if (spawner != null)
         {
-            spawner.SpawnAfterDelay(7f);
+            spawner.SpawnAfterDelay(3f);
 
         }
     }
@@ -49,8 +53,44 @@ public class testmob : MonoBehaviour
         Destroy(gameObject, 0.85f);
     }
 
+    private IEnumerator DamageOverTime()
+    {
+        while (currentHealth > 0)
+        {
+            int damage = Random.Range(1, 6);
+            TakeDamage(damage);
+            Debug.Log($"{currentHealth}");
+            if (currentHealth <= 0)
+            {
+                Die();
+                yield break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
     public bool getIsDead()
     {
         return isdead;
     }
+
+    void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        ShowDamageText(dmg);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void ShowDamageText(int damage)
+    {
+        GameObject dt = Instantiate(DamageTextPrefab, Vector3.zero, Quaternion.identity);
+        dt.transform.SetParent(GameObject.Find("Canvas").transform,false);
+        dt.transform.position = transform.position + Vector3.up * 1f;
+        dt.GetComponent<DamageText>().setDamageText(damage);
+    }
+    
+    
 }
