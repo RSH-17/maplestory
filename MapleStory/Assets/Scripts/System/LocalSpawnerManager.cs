306280a49fad81879ddbd6fd422a8faf9c975394
dongheon3 <kdh0304h@gmail.com>
2025-07-18 11:52:32 +0900

    test
    test

---
+++ MapleStory/Assets/Scripts/System/LocalSpawnerManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LocalSpawnerManager : MonoBehaviour
+{
+    //역할 : 몬스터 상태관리, 몬스터 스폰
+    void Awake()
+    {
+        Debug.Log("hihi");
+    }
+
+
+    void Update()
+    {
+
+    }
+    
+}
